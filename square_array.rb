67fe6b4808578d9527da0d66eccb77cbabff1282
Andrew Hinojosa <drews_view@hotmail.com>
2020-06-24 18:08:42 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   array.each do |each_element_of_array|
-    element = each_element_of_array*each_element_of_array
-    new_array << element
+    new_array << each_element_of_array*each_element_of_array
+    
  end 
 end
