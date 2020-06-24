964e1ed9d95d10ef851ebe3287f3bddece2183bc
Andrew Hinojosa <drews_view@hotmail.com>
2020-06-24 18:02:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,3 @@
 def square_array(array)
-  array.each {|each_element_of_array| puts "#{each_element_of_array*each_element_of_array}"}
+  array.each {|each_element_of_array| "#{each_element_of_array*each_element_of_array}"}
 end
